90c7043b9a1f307d1159e98149f69c59330bfa83
jessamelie <jessamelie.sola@gmail.com>
2023-05-19 14:42:09 +0200

    FIXED: bug popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React from "react";
 import "../style/Popup.css";
 
+const Popup = () => {
+  return (
+    <div className="popup-box">
+      <div className="box">
+        <div className="popup-container">
+          <div className="left-side">
+            <div className="login">
+              <h2>Se connecter</h2>
+              <input className="emailInput" placeholder="email"></input>
+              <input className="passwordInput" placeholder="password"></input>
+              <button className="loginBtn">Connexion</button>
+            </div>
+          </div>
 
-const Popup = (props) => {
-
-    return (
-        <div className="popup-box">
-            <div className='box'>
-                {props.content}
-            </div> 
+          <div className="right-side">
+            <div className="signUp">
+              <h2>S'inscrire</h2>
+              <button className="signupBtn">Inscription</button>
+            </div>
+            <div className="guest">
+              <h2>Continuer en tant qu'invité(e)</h2>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Popup;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,57 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "../style/Landing.css";
-import Popup from '../components/Popup';
-import Navigation from '../components/Navigation';
+import Popup from "../components/Popup";
+import Navigation from "../components/Navigation";
 
 const Landing = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false);
- 
-    const togglePopup = () => {
-      setIsOpen(!isOpen);
-    }
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <div>
-             <div className='background-container'>
-                <div className='title-container' onClick={togglePopup}>
-                <h1>SNKRS LAB </h1>
-                <div/> 
-            <div/>
-
-                <Navigation/>
-            
-                {isOpen && <Popup
-                      content={<>
-                    <div className='popup-container'>
-
-                        <div className='left-side'>
-                            <div className='login'>
-                                <h2>Se connecter</h2>
-                                <input className='emailInput' placeholder='email'></input>
-                                <input className='passwordInput' placeholder='password'></input>
-                                <button className='loginBtn'>Connexion</button>
-                            </div>
-                        </div>
-
-                        <div className='right-side'>
-                            <div className='signUp'>
-                                <h2>S'inscrire</h2>
-                                <button className='signupBtn'>Inscription</button>
-                            </div>
-                            <div className='guest'>
-                            <h2>Continuer en tant qu'invité(e)</h2>
-                            </div>
-                        </div>
-                    </div>
-                      </>}
-                      handleClose={togglePopup}
-                    />}
-                </div>
-         </div>
+  return (
+    <div>
+      <div className="background-container">
+        <div className="title-container" onClick={togglePopup}>
+          <h1>SNKRS LAB </h1>
         </div>
-
-    );
+      </div>
+      <Navigation />
+      {isOpen && <Popup handleClose={togglePopup} />}
+    </div>
+  );
 };
 
 export default Landing;
