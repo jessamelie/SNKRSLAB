4ffede34448549e0293b71f5f18154bd09b506b1
jessamelie <jessamelie.sola@gmail.com>
2023-06-16 15:06:41 +0200

    FIXED: popup and logo sync

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -11,12 +11,19 @@ const Landing = () => {
 
   return (
     <div>
-      <div className="background-container">
-        <div className="title-container" onClick={togglePopup}>
-          <h1>SNKRS LAB </h1>
-        </div>
+      <div className="title-container" onClick={togglePopup}>
+        <h1>SNKRS LAB </h1>
       </div>
+
+      <div className="background-container">
+        {isOpen || (
+        <div className="title-container" onClick={togglePopup}>
+        </div>
+        )}
+      </div>
+
       {isOpen && <Popup handleClose={togglePopup} />}
+
     </div>
   );
 };
