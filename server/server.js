01f308f5811dbd1cdfc713cd52b2cf2ffa9d27e5
jessamelie <jessamelie.sola@gmail.com>
2023-06-19 15:15:28 +0200

    ADD : routes folder

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 require('dotenv').config({path:'./config/.env'})
 const express = require('express');
+const userRoutes = require('./routes/user.routes.js');
 require('./config/database.js');
 const app = express();
 
 
+
+
+
+
+
+
+
+//Routes
+app.use('/api/user', userRoutes)
+
+
+//Appel serveur
 app.listen(process.env.PORT, () => (
     console.log(`Listening on port ${process.env.PORT}`)
 ))
