9e9acc395da3de9ef6eb281851c3721f2dedf2b7
jessamelie <jessamelie.sola@gmail.com>
2023-06-19 12:27:08 +0200

    CONFIG: configuration server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
-const express = require('express');
 require('dotenv').config({path:'./config/.env'})
+const express = require('express');
+require('./config/database.js');
 const app = express();
+
 
 app.listen(process.env.PORT, () => (
     console.log(`Listening on port ${process.env.PORT}`)
