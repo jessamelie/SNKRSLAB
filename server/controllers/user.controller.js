8dcd261e5a0433988b19c529887beb422f700dea
jessamelie <jessamelie.sola@gmail.com>
2023-06-20 16:45:02 +0200

    ADD : user database display (all user / one user info)

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,22 @@
 const userModel = require ('../models/user.model.js');
 const ObjectID = require('mongoose').Types.ObjectId;
 
-module.exports.getAllUsers = async (req, rest) => {
-    const users = await userModel.find().select();
-    rest.status(200).json(users);
+
+//Pour obtenir tous les users de la base de données 
+module.exports.getAllUsers = async (req, res) => {
+    const users = await userModel.find().select('-password'); //demande de tous les users sans les mdp
+    res.status(200).json(users);
 }
+
+//Pour obtbenir les infos d'un seul user
+module.exports.userInfo = async (req, res) => {
+    //pour savoir si l'ID est connu dans la base de données
+    if (!ObjectID.isValid(req.params.id)) 
+        return res.status(400).send('ID unknowm : ' + req.params.id)
+    
+    userModel.findById(req.params.id, (err, data) => {
+        if (!err) res.send(data);
+        else console.log('ID unknowm : ' + err)
+
+        }).select('-password')
+    };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -7,6 +7,7 @@ router.post('/register', authController.signUp);
 
 //User Display
 router.get('/', userController.getAllUsers);
+router.get('/:id', userController.userInfo);
 
 module.exports = router;
 
