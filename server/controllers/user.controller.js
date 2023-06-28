d98bdf74ea93f7c60cd8c06bb947733bac782766
jessamelie <jessamelie.sola@gmail.com>
2023-06-28 10:46:06 +0200

    ADD: follow part done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -59,15 +59,14 @@ module.exports.updateUser = async (req, res) => {
 //DELETE USER
 module.exports.deleteUser = async (req, res) => {
   try {
-
     if (!ObjectID.isValid(req.params.id)) {
       return res.status(400).send("Unknown ID: " + req.params.id);
     }
 
-    const user = await userModel.deleteOne({_id:req.params.id}).exec();
+    const user = await userModel.deleteOne({ _id: req.params.id }).exec();
 
     if (user) {
-      res.send({user:'Succesfully deleted'});
+      res.send({ user: "Succesfully deleted" });
     } else {
       res.status(404).send("User not found");
     }
@@ -75,4 +74,52 @@ module.exports.deleteUser = async (req, res) => {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
-}
+};
+
+//FOLLOW
+module.exports.follow = async (req, res) => {
+  try{
+    if (!ObjectID.isValid(req.params.id) || (!ObjectID.isValid(req.body.idToFollow))) {
+      return res.status(400).send("Unknown ID: " + req.params.id);
+    }
+
+//pour ajouter un abonné(e) dans sa liste (followers list)
+const user = await userModel.findByIdAndUpdate(req.params.id, 
+  {$addToSet: {following: req.body.idToFollow}},
+  {new: true, upsert: true});
+
+  if (user) {res.send(user)
+  } else { 
+  res.status(404).send("User not found") }
+
+//pour s'ajouter dans la liste des personnes que l'on suit (following list)
+await userModel.findByIdAndUpdate(req.body.idToFollow,
+  {$addToSet: {followers: req.params.id}},
+  {new: true, upsert: true});
+
+  // if (user) {res.send(user)
+  // } else { 
+  // res.status(404).send("User not found") }
+
+  }catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+
+//UNFOLLOW
+module.exports.unfollow = async (req, res) => {
+  try{
+    if (!ObjectID.isValid(req.params.id)) {
+      return res.status(400).send("Unknown ID: " + req.params.id);
+    }
+
+
+
+
+  }catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
