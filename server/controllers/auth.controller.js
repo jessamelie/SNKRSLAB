f2c54e4dd57766557c1b5688f447c7e727abdc06
jessamelie <jessamelie.sola@gmail.com>
2023-06-30 12:25:09 +0200

    Login & Json Web Token configuration

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
-//CREATE user
-// Gestion de l'inscription, la connexion et la déconnexion uniquement
+//Inscription
 module.exports.signUp = async (req, res) => {
   const {pseudo, email, password} = req.body;
 
@@ -12,3 +12,33 @@ module.exports.signUp = async (req, res) => {
     res.status(500).json({ error: 'Une erreur s\'est produite lors de l\'inscription' });
   }
 };
+
+//fonction pour créer le token
+const maxAge = 3 * 24 * 60 * 60 * 1000 //durée de validité du token
+const createToken = (id) => {
+  return jwt.sign({id}, process.env.TOKEN_SECRET, {
+    expiresIn: maxAge
+  })
+};
+
+//Connexion
+module.exports.signIn = async (req, res) => {
+const {email, password } = req.body
+
+try{
+  const user = await userModel.login(email, password);
+  //création de token
+  const token = createToken(user._id);
+  res.cookie('jwt', token, {httpOnly: true, maxAge}); //jwt est le nom du cookie
+  res.status(200).json({user: user._id})
+} catch (err){
+  res.status(200).json(err)
+}
+};
+
+//Déconnexion
+//Le principe est de retiré le token qui a été donné à l'user puis de le rediriger sur une autre page
+module.exports.logOut = async (req,res) => {
+  res.cookie('jwt','', {maxAge:1});
+  res.redirect('/');
+}
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -44,12 +44,25 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     });
 
-//Avant d'enregistrer les données dans la DB, cryptage du mot de passe
+//Avant d'enregistrer les données dans la DB, salage du mot de passe avant de rentrer l'user
 userSchema.pre('save', async function(next) {
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
     next();
-})
+});
+
+//désalage du password afin de comparer l'input de l'user à ce qu'il y a dans la DB
+userSchema.statics.login = async function(email, password){
+    const user = await this.findOne({email});
+    if (user){
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+            return user;
+        }
+        throw Error('incorret password')
+    }
+    throw Error('incorrect email')
+}
 
 const userModel = mongoose.model('user',userSchema);
 module.exports = userModel
