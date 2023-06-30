14338a31f61194869f7bfbd096b9f30faa701e18
jessamelie <jessamelie.sola@gmail.com>
2023-06-30 12:25:09 +0200

    Login & Json Web Token configuration

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
-//CREATE user
-// Gestion de l'inscription, la connexion et la déconnexion uniquement
+//Inscription
 module.exports.signUp = async (req, res) => {
   const {pseudo, email, password} = req.body;
 
@@ -12,3 +12,31 @@ module.exports.signUp = async (req, res) => {
     res.status(500).json({ error: 'Une erreur s\'est produite lors de l\'inscription' });
   }
 };
+
+//fonction pour créer le token
+const maxAge = 3 * 24 * 60 * 60 * 1000 //durée de validité du token
+const createToken = (id) => {
+  return jwt.sign({id}, process.env.TOKEN_SECRET, {
+    expiresIn: maxAge
+  })
+};
+
+//Connexion
+module.exports.signIn = async (req, res) => {
+const {email, password } = req.body
+
+try{
+  const user = await userModel.login(email, password);
+  //création de token
+  const token = createToken(user._id);
+  res.cookie('jwt', token, {httpOnly: true, maxAge}); //jwt est le nom du cookie
+  res.status(200).json({user: user._id})
+} catch (err){
+  res.status(200).json(err)
+}
+};
+
+//Déconnexion
+module.exports.logOut = async (req,res) => {
+
+}
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,6 +4,8 @@ const userController = require('../controllers/user.controller.js');
 
 //Authentification
 router.post('/register', authController.signUp);
+router.post('/login', authController.signIn);
+router.get('/logout', authController.logOut);
 
 //User Display
 router.get('/', userController.getAllUsers);
@@ -12,6 +14,6 @@ router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 router.patch('/follow/:id', userController.follow);
 router.patch('/unfollow/:id', userController.unfollow);
-
+ 
 module.exports = router;
 
